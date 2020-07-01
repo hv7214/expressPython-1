36e5e81205bea705455adead4d3a768e32eb3511
Harshit Verma <hverma1@cs.iitr.ac.in>
2020-07-01 23:58:55 +0530

    update(appveyor.yml): added antlr build

--- ANTLR4runtime/runtime/src/antlr4-common.h
+++ ANTLR4runtime/runtime/src/antlr4-common.h
@@ -53,8 +53,8 @@
 
   #ifdef _WIN64
     typedef __int64 ssize_t;
-  #else
-    typedef __int32 ssize_t;
+  // #else
+  //   typedef __int32 ssize_t;
   #endif
 
   #if _MSC_VER >= 1900 && _MSC_VER < 2000
